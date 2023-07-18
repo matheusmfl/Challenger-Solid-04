753023428c05763517933eaa4283b4de726c9186
Matheus Fonteles <92957086+matheusmfl@users.noreply.github.com>
2023-07-18 18:50:03 -0300

    Updated controller listAllUser

--- src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
@@ -8,6 +8,8 @@ class ListAllUsersController {
   handle(request: Request, response: Response): Response {
     const { user_id } = request.params
 
+    this.listAllUsersUseCase.execute({ user_id })
+
     return response.status(200).send()
   }
 }
